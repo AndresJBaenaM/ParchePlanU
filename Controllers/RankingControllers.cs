3692f84871ba0014f76313f189785cc4d310e9c8
Miguel Gómez Tobón <miguel.gomez35@eia.edu.co>
2026-03-16 13:31:46 -0500

    Se crean los Controllers

--- Controllers/RankingControllers.cs
+++ Controllers/RankingControllers.cs
@@ -1,6 +1,26 @@
-﻿namespace ApiParchePlanU.Controllers
+﻿using ApiParchePlanU.Interfaces;
+using ApiParchePlanU.Models;
+using ApiParchePlanU.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiParchePlanU.Controllers
 {
-    public class RankingControllers
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RankingController : ControllerBase
     {
+        private readonly IRankingServices _rankingService;
+
+        public RankingController(IRankingServices rankingService)
+        {
+            _rankingService = rankingService;
+        }
+
+        [HttpGet("{parcheId}")]
+        public async Task<ActionResult<List<Ranking>>> GetRanking(int parcheId)
+        {
+            var ranking = await _rankingService.GetRanking(parcheId);
+            return Ok(ranking);
+        }
     }
 }
